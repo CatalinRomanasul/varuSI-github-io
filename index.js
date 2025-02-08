a71e8a20eb1b5341a0c32a1077ea9f222362ed00
CatalinRomanasul <catalinmocanu116@gmail.com>
2025-02-08 03:58:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ buton1.addEventListener('click',()=>{
   renderZone.innerHTML ='';
   for(i=1;i<=100;i++)
   {if(i<10)
-    renderZone.innerHTML+=`<a target="_blank" href="fise1/e_f_electricitate_si_00${i}.pdf">Fisa ${i}</a>`
+    renderZone.innerHTML+=`<a target="_blank" href="fisa1/e_f_electricitate_si_00${i}.pdf">Fisa ${i}</a>`
     else if(i<100)
-      renderZone.innerHTML+=`<a target="_blank" href="fise1/e_f_electricitate_si_0${i}.pdf">Fisa ${i}</a>`
+      renderZone.innerHTML+=`<a target="_blank" href="fisa1/e_f_electricitate_si_0${i}.pdf">Fisa ${i}</a>`
     else
     
-      renderZone.innerHTML+=`<a target="_blank" href="fise1/e_f_electricitate_si_${i}.pdf">Fisa ${i}</a>`
+      renderZone.innerHTML+=`<a target="_blank" href="fisa1/e_f_electricitate_si_${i}.pdf">Fisa ${i}</a>`
   }
 
 })
@@ -22,12 +22,12 @@ buton2.addEventListener('click',()=>{
   renderZone.innerHTML ='';
   for(i=1;i<=100;i++)
   {if(i<10)
-    renderZone.innerHTML+=`<a target="_blank" href="fise2/e_f_electricitate_sii_00${i}.pdf">Fisa ${i}</a>`
+    renderZone.innerHTML+=`<a target="_blank" href="fisa2/e_f_electricitate_sii_00${i}.pdf">Fisa ${i}</a>`
     else if(i<100)
-      renderZone.innerHTML+=`<a target="_blank" href="fise2/e_f_electricitate_sii_0${i}.pdf">Fisa ${i}</a>`
+      renderZone.innerHTML+=`<a target="_blank" href="fisa2/e_f_electricitate_sii_0${i}.pdf">Fisa ${i}</a>`
     else
     
-      renderZone.innerHTML+=`<a target="_blank" href="fise2/e_f_electricitate_sii_${i}.pdf">Fisa ${i}</a>`
+      renderZone.innerHTML+=`<a target="_blank" href="fisa2/e_f_electricitate_sii_${i}.pdf">Fisa ${i}</a>`
   }
 
 })
@@ -38,12 +38,12 @@ buton3.addEventListener('click',()=>{
   renderZone.innerHTML ='';
   for(i=1;i<=100;i++)
   {if(i<10)
-    renderZone.innerHTML+=`<a target="_blank" href="fise3/e_f_electricitate_siii_00${i}.pdf">Fisa ${i}</a>`
+    renderZone.innerHTML+=`<a target="_blank" href="fisa3/e_f_electricitate_siii_00${i}.pdf">Fisa ${i}</a>`
     else if(i<100)
-      renderZone.innerHTML+=`<a target="_blank" href="fise3/e_f_electricitate_siii_0${i}.pdf">Fisa ${i}</a>`
+      renderZone.innerHTML+=`<a target="_blank" href="fisa3/e_f_electricitate_siii_0${i}.pdf">Fisa ${i}</a>`
     else
     
-      renderZone.innerHTML+=`<a target="_blank" href="fise3/e_f_electricitate_siii_${i}.pdf">Fisa ${i}</a>`
+      renderZone.innerHTML+=`<a target="_blank" href="fisa3/e_f_electricitate_siii_${i}.pdf">Fisa ${i}</a>`
   }
 
 })
